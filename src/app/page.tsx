d30c9b562e589f446783b8be2a9317a1676dad5e
Bintang Dhiya A <bintang.dhiya@gmail.com>
2025-08-10 17:28:07 +0700

    Fix footer component move into layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from '@/components/HeroSection';
 import ServiceHighlight from '@/components/ServiceHighlight';
 import StatSection from "@/components/StatsSection"
 import NewsSection from '@/components/NewsSection';
-import FooterSection from '@/components/FooterSection';
+
 export default function Home() {
   return (
     <main className="min-h-screen">
@@ -12,7 +12,6 @@ export default function Home() {
       <ServiceHighlight />
       <StatSection/>
       <NewsSection/>
-      <FooterSection/>
     </main>
   );
 }
