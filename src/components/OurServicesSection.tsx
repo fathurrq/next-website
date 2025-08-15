8fd59e1a6e1146403b8ad5905635ec856e55b14c
Rifqy Zacky Ariadhy <rifqy.zacky@runchise.com>
2025-08-15 17:04:45 +0700

    add responsive adjustments to "Our Services" section

--- src/components/OurServicesSection.tsx
+++ src/components/OurServicesSection.tsx
@@ -84,23 +84,23 @@ export default function OurServicesSection() {
                 {showOurServicesTitle && (
                     <motion.div
                         key="our-services-title"
-                        className="absolute left-1/2 top-1/6 -translate-x-1/2 -translate-y-1/2 z-30 text-center px-4"
+                        className="absolute left-1/2 top-1/8 xl:top-1/6 -translate-x-1/2 -translate-y-1/2 z-30 text-center px-4"
                         initial={{ opacity: 0, y: 1000 }}
                         animate={{opacity: 1, y: 0, transition: {duration: 1.5, ease: "easeOut"}}}
                         exit={{ opacity: 0, transition: { duration: 0.35 } }}
                     >
-                        <h1 className="text-6xl md:text-8xl text-white">Our Services</h1>
+                        <h1 className="text-6xl xl:text-8xl text-white">Our Services</h1>
                     </motion.div>
                 )}
                 {showServicesDescription && (
                     <motion.div
                         key="our-services-description"
-                        className="absolute left-0 top-1/5 z-30 text-center px-4 flex items-center justify-center"
+                        className="absolute left-0 top-1/6 xl:top-1/5 z-30 text-center px-4 flex items-center justify-center"
                         initial={{ opacity: 0, y: 1000 }}
                         animate={{ opacity: 1, y: 0, transition: { duration: 1.5, ease: "easeOut" } }}
                         exit={{ opacity: 0, transition: { duration: 0.35 } }}
                     >
-                        <p className="text-2xl md:text-[32px] text-white font-medium w-2/3">
+                        <p className="text-2xl xl:text-[32px] text-white font-medium w-2/3">
                             Step into a world where every vessel is guided by precision and trust. From design approvals to on-board inspections, we ensure each ship meets the highest global standards.
                             <br /><br />
                             Safeguarding lives, protecting the environment, and enhancing operational performance—our expertise keeps Indonesia’s maritime industry moving forward.
@@ -114,7 +114,7 @@ export default function OurServicesSection() {
             <motion.img src="/services-bg.png" alt="Background" className="w-full h-full object-cover" />
             <div className={'w-full h-[100vh] bg-black'}></div>
             <div
-                className="absolute inset-x-0 top-[175vh] z-20 pointer-events-none h-[100vh] w-full flex items-center justify-center"
+                className="absolute inset-x-0 top-[130vh] xl:top-[175vh] z-20 pointer-events-none xl:h-[100vh] w-full flex items-center justify-center"
                 style={{
                     background:
                         "linear-gradient(0deg,rgba(0, 0, 0, 1) 0%, rgba(10, 67, 106, 1) 83%, rgba(0, 0, 0, 0) 100%)",
@@ -125,28 +125,28 @@ export default function OurServicesSection() {
                     {showOurServicesBottomTitle && (
                         <motion.div
                             key="our-services-title-bottom"
-                            className="absolute left-1/2 top-1/4 -translate-x-1/2 -translate-y-1/2 z-30 text-center px-4"
+                            className="absolute left-1/2 top-1/5 xl:top-1/4 -translate-x-1/2 -translate-y-1/2 z-30 text-center px-4"
                             initial={{ opacity: 0, y: -1000 }}
                             animate={{opacity: 1, y: 0, transition: {duration: 1.5, ease: "easeOut"}}}
                             exit={{ opacity: 0, transition: { duration: 0.35 } }}
                         >
-                            <h1 className="text-6xl md:text-8xl text-[rgba(255,255,255,0.5)]">Our Services</h1>
+                            <h1 className="text-6xl xl:text-8xl text-[rgba(255,255,255,0.5)]">Our Services</h1>
                         </motion.div>
                     )}
                     {showOurServicesBottomTitle && (
                         <motion.div
                             key="our-services-description-bottom"
-                            className="absolute left-0 top-2/5 z-30 text-center px-4 flex items-center justify-center w-full"
+                            className="absolute left-0 top-1/4 xl:top-2/5 z-30 text-center px-4 flex items-center justify-center w-full"
                             initial={{ opacity: 0 }}
                             animate={{opacity: 1, transition: {duration: 1.5, ease: "easeOut"}}}
                             exit={{ opacity: 0, transition: { duration: 0.35 } }}
                         >
-                            <p className="text-3xl md:text-[40px] text-[rgba(255,255,255,0.5)]">Discover maritime services built for safety, compliance, and excellence.</p>
+                            <p className="text-3xl xl:text-[40px] text-[rgba(255,255,255,0.5)]">Discover maritime services built for safety, compliance, and excellence.</p>
                         </motion.div>
                     )}
                 </AnimatePresence>
                 <div
-                    className={'grid grid-cols-1 md:grid-cols-3 gap-x-[30px] gap-y-[60px] mt-[50vh] mx-[50px] md:mx-[70px]'}>
+                    className={'grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-[30px] gap-y-[60px] mt-[60vh] xl:mt-[50vh] mx-[50px] xl:mx-[70px] pb-[100px] xl:pb-0'}>
                     {services.map((service, index) => (
                         <div key={index} className={`py-[20px] px-[32px] rounded-[4px] relative group`}>
                             <Image
@@ -158,8 +158,8 @@ export default function OurServicesSection() {
                             />
                             <div className={'flex flex-col gap-[15px] z-1'}>
                                 <Image src={service.icon} alt={service.title} width={32} height={22}/>
-                                <p className={'text-4xl md:text-5xl text-white mt-[15px]'}>{service.title}</p>
-                                <p className={'text-sm md:text-[16px] text-white'}>{service.description}</p>
+                                <p className={'text-4xl xl:text-5xl text-white mt-[15px]'}>{service.title}</p>
+                                <p className={'text-sm xl:text-[16px] text-white'}>{service.description}</p>
                             </div>
                             <div className={'absolute bottom-0 left-[32px] right-[32px] h-[1px] bg-white opacity-30 z-0'} />
                             {/*<div className={'absolute bottom-0 left-[32px] right-[32px] h-[1px] bg-white opacity-80 z-1'} />*/}
