85d2170b8c4a6656dc30ddf64961cb4c9c5e4193
Bintang Dhiya A <bintang.dhiya@gmail.com>
2025-08-15 14:29:30 +0700

    fix: newsletter font size

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,18 +1,18 @@
 export default function Newsletter() {
   return (
-    <div className="py-36 px-6 md:px-12 lg:px-48">
+    <div className="py-36 md:px-12 lg:px-48">
       <div className="text-white border-white/50 border-opacity-50 rounded flex items-center py-8 flex-col gap-8 border-4 backdrop-blur-xl w-full max-w-3xl mx-auto">
-        <div className="font-medium text-3xl md:text-6xl text-center">
+        <div className="font-medium text-xl md:text-3xl text-center">
           Subscribe to Our Newsletter
         </div>
-        <p className="text-lg md:text-2xl text-white/50 text-center">
+        <p className="text-sm md:text-lg text-white/50 text-center">
           News, promotion, and exclusive content.
         </p>
-        <div className="w-full px-28">
+        <div className="w-full px-8 md:px-28">
           <input
             type="text"
             placeholder="Enter email here"
-            className="w-full text-center bg-transparent outline-none border-b border-white text-lg md:text-3xl placeholder:text-lg md:placeholder:text-3xl"
+            className="w-full text-center bg-transparent outline-none border-b border-white text-sm md:text-lg placeholder:text-lg md:placeholder:text-3xl"
           />
         </div>
         <a
