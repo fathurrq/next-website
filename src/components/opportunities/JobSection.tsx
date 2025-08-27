580c98af795dee56e558623f039a58bc1c65c58e
Bintang Dhiya A <bintang.dhiya@gmail.com>
2025-08-27 11:47:28 +0700

    feat: opportunity job section responsive

--- src/components/opportunities/JobSection.tsx
+++ src/components/opportunities/JobSection.tsx
@@ -49,21 +49,21 @@ export default function JobSection() {
     return (
         <div
             id="job-opportunities"
-            className="font-montserrat text-black flex flex-col w-full h-auto p-16 2xl:p-24 gap-12 2xl:gap-16 bg-gradient-to-b from-[#FFFFFF] to-[#BF9F5F]"
+            className="font-montserrat text-black flex flex-col w-full h-auto p-4 md:p-16 2xl:p-24 gap-12 2xl:gap-16 bg-gradient-to-b from-[#FFFFFF] to-[#BF9F5F]"
         >
             <div className="w-full h-auto flex flex-col justify-center items-center gap-12 px-4 2xl:px-6 py-12 2xl:py-16 bg-white shadow-md">
                 <div className="flex flex-col items-center text-center gap-3 2xl:gap-5">
-                    <h3 className="font-bold text-[#BF9F5F] text-3xl 2xl:text-5xl">Job Opportunities</h3>
-                    <p className="">BKI believe putting the best people in accordance with the interests and abilities are key in regulating human resources.
+                    <h3 className="font-bold text-[#BF9F5F] text-xl md:text-3xl 2xl:text-5xl">Job Opportunities</h3>
+                    <p className="text-xs md:text-base 2xl:text-lg">BKI believe putting the best people in accordance with the interests and abilities are key in regulating human resources.
                         To that end, we provide opportunities for qualified employees to continue to develop according to their interest and ability to move forward with us.</p>
                 </div>
 
                 <div className="w-full h-auto flex flex-col bg-white border border-gray-300">
-                    <h3 className="font-bold text-[#0A436A] text-3xl 2xl:text-5xl 2xl:py-6 2xl:px-4 py-4 px-2">Head Office - Jakarta</h3>
+                    <h3 className="font-bold text-[#0A436A] text-xl md:text-3xl 2xl:text-5xl 2xl:py-6 2xl:px-4 py-4 px-2">Head Office - Jakarta</h3>
                     {accordionData.map((item, index) => (
                         <AccordionItem key={index} title={item.title}>
                             <div>
-                                <h4 className="font-semibold mb-2">Deskripsi:</h4>
+                                <h4 className="font-semibold mb-2 text-lg md:text-2xl 2xl:text-4xl">Deskripsi:</h4>
                                 <p className="mb-2">
                                     {item.description}
                                 </p>
@@ -76,7 +76,7 @@ export default function JobSection() {
                                     </ul>)}
 
                                 {item.qualifications && item.qualifications.length > 0 && (<>
-                                    <h4 className="font-semibold mb-2">Kualifikasi:</h4>
+                                    <h4 className="font-semibold mb-2 text-lg md:text-2xl 2xl:text-4xl">Kualifikasi:</h4>
                                     <ul className="list-disc list-inside mb-4">
                                         {item.qualifications.map((di, dx) => (
                                             <li key={dx}>{di}</li>
@@ -85,7 +85,8 @@ export default function JobSection() {
                                 </>)}
 
                                 <div className="flex justify-end">
-                                    <Link href="/apply" className="px-4 py-2 bg-[#0A436A] text-white rounded-lg shadow hover:bg-[#08324f] transition">
+                                    <Link href="/apply" className="px-4 py-2 bg-[#0A436A] text-white rounded-sm shadow hover:bg-[#08324f] transition
+                                    text-lg md:text-2xl 2xl:text-4xl">
                                         Apply
                                     </Link>
                                 </div>
